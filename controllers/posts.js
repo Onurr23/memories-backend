3509446cc7df10109e91d3620508218fa154adb1
Onur <onur.kara@obss.com.tr>
2022-10-30 11:21:17 +0300

    update post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,4 +19,17 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    res.status(404).json({ message: "No post with that id" });
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.status(201).json(updatedPost);
 };
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ import { createPost, getPosts } from "../controllers/posts.js";
 const router = express.Router();
 
 router.route("/").get(getPosts).post(createPost);
+router.route(":/id").patch(updatePost);
 
 export default router;
